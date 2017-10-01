78e15dc07b79960c2423c8658ba197d2ad6b0b70
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-01 18:34:40 +0300

    Added XML serializable ApiDecl classes.

---
+++ Library/Context/Url Generation/ApiDecl/Declaration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BuyBuddy.Context.UrlGeneration.ApiDecl {
+    [XmlRoot]
+    public sealed class Declaration {
+        [XmlElement]
+        public Scope scope;
+    }
+}
---
+++ Library/Context/Url Generation/ApiDecl/Scope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BuyBuddy.Context.UrlGeneration.ApiDecl {
+    [XmlRoot]
+    public sealed class Scope {
+        [XmlAttribute]
+        public string Name;
+
+        [XmlAttribute]
+        public string PermissionPrefix;
+
+        [XmlAttribute]
+        public string Path;
+
+        [XmlArray]
+        public Route routes;
+    }
+}
