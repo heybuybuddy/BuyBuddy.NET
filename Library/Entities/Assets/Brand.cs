23003161a54ba51a3aee659544e4594e7a2ce30d
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-02 02:32:36 +0300

    Created entities for assets.

---
+++ Library/Entities/Assets/Brand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuyBuddy.Entities.Assets {
+    public sealed class Brand {
+        public int Id { get; private set; }
+    }
+}
---
+++ Library/Entities/Assets/Store.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuyBuddy.Entities.Assets {
+    public sealed class Store {
+        public int Id { get; private set; }
+    }
+}
