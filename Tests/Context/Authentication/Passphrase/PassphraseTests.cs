ca0a702e90f89b6ace6f2981c5bee75a5a984b58
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-01 18:33:59 +0300

    Added test for passphrases.

---
+++ Tests/Context/Authentication/Passphrase/PassphraseTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuyBuddyTests.Context.Authentication {
+    [TestClass]
+    public class PassphraseTests {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
