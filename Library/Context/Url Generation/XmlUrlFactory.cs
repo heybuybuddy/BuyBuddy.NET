00551d771fe8ab5da90ca1baf8498d83f0156158
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-01 18:36:00 +0300

    Added concrete implementation for UrlFactory with XML.

---
+++ Library/Context/Url Generation/XmlUrlFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace BuyBuddy.Context.UrlGeneration {
+    internal class XmlUrlFactory : UrlFactory {
+        public static readonly string Path = "ApiRoutes.xml";
+        public ApiDecl.Declaration Declaration { get; private set; }
+
+        internal XmlUrlFactory(string baseUrl)
+            : base(baseUrl) {
+            var serializer = new XmlSerializer(typeof(ApiDecl.Declaration));
+            var fs = new FileStream(Path, FileMode.Open);
+
+            Declaration = serializer.Deserialize(fs) as ApiDecl.Declaration;
+        }
+    }
+}
