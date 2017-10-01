a509244df11224e26cabc008fb23678fb56a370c
Buğra Ekuklu <ekuklu@icloud.com>
2017-10-01 18:35:48 +0300

    Added UrlFactory abstract class.

---
+++ Library/Context/Url Generation/UrlFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuyBuddy.Context.UrlGeneration {
+    internal abstract class UrlFactory {
+        internal string BaseUrl { get; private set; }
+
+        internal UrlFactory(string baseUrl) {
+            BaseUrl = baseUrl;
+        }
+    }
+}
